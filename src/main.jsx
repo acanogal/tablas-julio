fa886703de5b1d07d607441670e7f4e50a2c6b2b
Antonio Cano <acanogal@gmail.com>
2026-02-07 21:31:47 +0100

    feat: integrate PWA support with service worker registration and caching configuration

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { registerSW } from 'virtual:pwa-register'
 import { I18nProvider } from './i18n.jsx'
 import './index.css'
 import App from './App.jsx'
+
+registerSW({
+  onNeedRefresh() {
+    window.location.reload()
+  },
+  onOfflineReady() {},
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
